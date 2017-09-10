22e8c73b93f93afc799ca4b6519c663d0a1c8976
Alexandre Berard <eskaev@gmail.com>
2017-09-10 16:11:30 +0200

    Improved plot script

--- scripts/plot-loss.py
+++ scripts/plot-loss.py
@@ -10,14 +10,21 @@ parser.add_argument('--output')
 parser.add_argument('--max-steps', type=int, default=0)
 parser.add_argument('--min-steps', type=int, default=0)
 parser.add_argument('--labels', nargs='+')
-parser.add_argument('--plot', nargs='+')
-parser.add_argument('--average', type=int, nargs='+')
-parser.add_argument('--smooth', type=int)
+
 parser.add_argument('--no-x', action='store_true', help='Run with no X server')
+
 parser.add_argument('--txt', action='store_true')
 parser.add_argument('--stride', type=int)
 parser.add_argument('-n', '--max-values', type=int, default=15)
 parser.add_argument('--shortest', action='store_true')
+parser.add_argument('--avg', action='store_true')
+parser.add_argument('--best', action='store_true')
+
+parser.add_argument('--plot', nargs='+', default=[])
+parser.add_argument('--bleu', action='store_true')
+parser.add_argument('--ter', action='store_true')
+parser.add_argument('--dev-loss', action='store_true')
+parser.add_argument('--train-loss', action='store_true')
 
 args = parser.parse_args()
 
@@ -25,11 +32,19 @@ args = parser.parse_args()
 args.log_files = [os.path.join(log_file, 'log.txt') if os.path.isdir(log_file) else log_file
                   for log_file in args.log_files]
 
+args.plot = [x.lower() for x in args.plot]
+
+if args.bleu and 'bleu' not in args.plot:
+    args.plot.append('bleu')
+if args.ter and 'ter' not in args.plot:
+    args.plot.append('ter')
+if args.dev_loss and 'dev' not in args.plot:
+    args.plot.append('dev')
+if args.train_loss and 'train' not in args.plot:
+    args.plot.append('train')
+
 if not args.plot:
-    if args.txt:
-        args.plot = ('bleu',)
-    else:
-        args.plot = ('dev', 'train')
+    args.plot = ['bleu'] if args.txt else ['dev', 'train']
 
 if not args.txt:
     try:
@@ -41,16 +56,9 @@ if not args.txt:
         sys.stderr.write('failed to import matplotlib: reverting to txt mode\n')
         args.txt = True
 
-args.plot = [x.lower() for x in args.plot]
-
-if args.average:
-    assert sum(args.average) == len(args.log_files)
-
-n = len(args.average) if args.average else len(args.log_files)
-
 labels = None
 if args.labels:
-    if len(args.labels) != n:
+    if len(args.labels) != len(args.log_files):
         raise Exception('error: wrong number of labels')
     labels = args.labels
 
@@ -64,7 +72,7 @@ if not labels:
     if all(filenames) and len(set(filenames)) == len(filenames):
         labels = filenames
 
-labels = labels or ['model {}'.format(i) for i in range(1, n + 1)]
+labels = labels or ['model {}'.format(i) for i in range(1, len(args.log_files) + 1)]
 data = []
 
 for log_file in args.log_files:
@@ -81,10 +89,10 @@ for log_file in args.log_files:
             if m:
                 current_step = int(m.group(1))
 
-            if 0 < args.max_steps < current_step:
-                break
-            if current_step < args.min_steps:
-                continue
+            # if 0 < args.max_steps < current_step:
+            #     break
+            # if current_step < args.min_steps:
+            #     continue
 
             m = re.search(r'eval: loss (-?\d+.\d+)', line)
             if m and not any(step == current_step for step, _ in dev_perplexities):
@@ -112,33 +120,9 @@ for log_file in args.log_files:
     data.append((bleu_scores, ter_scores, dev_perplexities, train_perplexities))
 
 
-if args.average:
-    new_data = []
-
-    i = 0
-    for n in args.average:
-        data_ = zip(*data[i:i + n])
-        i += n
-
-        def avg(data_):
-            dicts = [dict(l) for l in data_]
-            keys = set.intersection(*[set(d.keys()) for d in dicts])
-            data_ = {k: (sum(d[k] for d in dicts) / n) for k in keys}
-            data_ = sorted(list(data_.items()))
-
-            if args.smooth is not None and args.smooth > 1:
-                k = args.smooth
-                data_ = [(data_[i*k][0], sum(x for _, x in data_[i*k:(i+1)*k]) / k) for i in range(len(data_) // k)]
-
-            return data_
-
-        new_data.append(list(map(avg, data_)))
-    data = new_data
-
-
 score_names = ['bleu', 'ter', 'dev', 'train']
 score_labels = ['BLEU', 'TER', 'dev loss', 'train loss']
-linestyles = [':', ':', '--', None]
+linestyles = [':', ':', '--', '--']
 
 def boldify(text):
     return '\033[1m' + text + '\033[0m'
@@ -165,8 +149,15 @@ if args.txt:
         else:
             steps = sorted(list(set.union(*steps)))
 
+        steps = [step for step in steps if step >= args.min_steps]
+        steps = [step for step in steps if args.max_steps == 0 or step <= args.max_steps]
+
         if args.stride:
-            steps = steps[args.stride - 1::args.stride]
+            if args.min_steps:  # we want to include the first value
+                steps = steps[::args.stride]
+            else:
+                steps = steps[args.stride - 1::args.stride]
+
         if args.max_values:
             steps = steps[:args.max_values]
 
@@ -174,17 +165,34 @@ if args.txt:
         
         print(fmt.format(score_label), ''.join('{:>7}'.format(step) for step in steps))
         for model_label, values_ in zip(labels, values):
-            values_ = dict(values_)
-            values_ = [values_.get(step) for step in steps]
+            values__ = []
+            for min_step, max_step in zip([-1] + steps, steps):
+                a = [value for step, value in values_ if min_step < step <= max_step]
+                if not a:
+                    a = None
+                elif args.best and score_name == 'bleu':
+                    a = max(a)
+                elif args.best:
+                    a = min(a)
+                elif args.avg:
+                    a = sum(a) / len(a)
+                else:
+                    a = a[-1]
+                values__.append(a)
+            values_ = values__
+
             try:
                 best_value = max(filter(None, values_)) if score_name == 'bleu' else min(filter(None, values_))
             except ValueError:
                 best_value = 0
-            s = ['{:7>}'.format('') if x is None else '{:>7.2f}'.format(x) for x in values_]
+            s = ['{:>7}'.format('') if x is None else '{:>7.2f}'.format(x) for x in values_]
             s = [boldify(y) if x == best_value else y for x, y in zip(values_, s)]
             print(fmt.format(model_label), ''.join(s))
 else:
     for label, data_ in zip(labels, data):
+        data_ = [[(step, value) for step, value in data__ if step >= args.min_steps
+                  and (args.max_steps == 0 or step <= args.max_steps)] for data__ in data_]
+
         for score_name, score_label, linestyle, values in zip(score_names, score_labels, linestyles, data_):
             if score_name in args.plot and values:
                 plt.plot(*zip(*values), label=' '.join([label, score_label]), linestyle=linestyle)
---
+++ scripts/ssh-plot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -e
+
+rm -f /tmp/plot.svg
+ssh $1 "rm -f /tmp/plot.svg && plot-loss ${@:2} --output /tmp/plot.svg --no-x"
+scp $1:/tmp/plot.svg /tmp/ >/dev/null 2>&1
+if [ -f /tmp/plot.svg ]; then
+    eog /tmp/plot.svg >/dev/null 2>&1 &
+fi
+
