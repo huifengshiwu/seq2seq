fd798de12c8fd58a19b3826b3b3cf8fc91b53086
eske <eskaev@gmail.com>
2017-09-14 14:41:09 +0200

    Improved plotting script

--- scripts/plot-loss.py
+++ scripts/plot-loss.py
@@ -3,6 +3,8 @@ import argparse
 import re
 import os
 import sys
+import itertools
+from collections import OrderedDict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('log_files', nargs='+')
@@ -15,7 +17,7 @@ parser.add_argument('--no-x', action='store_true', help='Run with no X server')
 
 parser.add_argument('--txt', '--text', action='store_true')
 parser.add_argument('--stride', type=int)
-parser.add_argument('-n', '--max-values', type=int, default=15)
+parser.add_argument('-n', type=int, default=15, dest='max_values')
 parser.add_argument('--shortest', action='store_true')
 parser.add_argument('--avg', action='store_true')
 parser.add_argument('--best', action='store_true')
@@ -46,6 +48,12 @@ if args.train_loss and 'train' not in args.plot:
 if not args.plot:
     args.plot = ['bleu'] if args.txt else ['dev', 'train']
 
+args.bleu = 'bleu' in args.plot
+args.ter = 'ter' in args.plot
+args.dev = 'dev' in args.plot
+args.dev_loss = 'dev' in args.plot
+args.train_loss = 'train' in args.plot
+
 if not args.txt:
     try:
         import matplotlib
@@ -73,7 +81,9 @@ if not labels:
         labels = filenames
 
 labels = labels or ['model {}'.format(i) for i in range(1, len(args.log_files) + 1)]
-data = []
+data = OrderedDict()
+for name in args.plot:
+    data[name] = []
 
 for log_file in args.log_files:
     current_step = 0
@@ -88,11 +98,6 @@ for log_file in args.log_files:
             m = re.search('step (\d+)', line)
             if m:
                 current_step = int(m.group(1))
-
-            # if 0 < args.max_steps < current_step:
-            #     break
-            # if current_step < args.min_steps:
-            #     continue
 
             m = re.search(r'eval: loss (-?\d+.\d+)', line)
             if m and not any(step == current_step for step, _ in dev_perplexities):
@@ -117,28 +122,34 @@ for log_file in args.log_files:
                 ter_score = float(m.group(1))
                 ter_scores.append((current_step, ter_score))
 
-    data.append((bleu_scores, ter_scores, dev_perplexities, train_perplexities))
+    if 'ter' in data:
+        data['ter'].append(ter_scores)
+    if 'bleu' in data:
+        data['bleu'].append(bleu_scores)
+    if 'dev' in data:
+        data['dev'].append(dev_perplexities)
+    if 'train' in data:
+        data['train'].append(train_perplexities)
 
-
-score_names = ['bleu', 'ter', 'dev', 'train']
-score_labels = ['BLEU', 'TER', 'dev loss', 'train loss']
-linestyles = [':', ':', '--', '--']
+metric_labels = {
+    'bleu': 'BLEU',
+    'ter': 'TER',
+    'dev': 'dev loss',
+    'train': 'train loss'
+}
 
 def boldify(text):
     return '\033[1m' + text + '\033[0m'
 
 if args.txt:
-    data = list(zip(*data))
+    # data = list(zip(*data))
 
-    l = max(len(label) for name, label in zip(score_names, score_labels) if name in args.plot)
+    l = max(len(metric_labels[name]) for name in data)
     l = max(l, max(map(len, labels)))
     fmt = '{{:<{}}}'.format(l)
 
     i = 0
-    for score_name, score_label, values in zip(score_names, score_labels, data):
-        if score_name not in args.plot or not values or not any(values):
-            continue
-
+    for name, values in data.items():
         if i > 0:
             print()
         i += 1
@@ -163,14 +174,14 @@ if args.txt:
 
         steps_ = set(steps)
         
-        print(fmt.format(score_label), ''.join('{:>7}'.format(step) for step in steps))
+        print(fmt.format(metric_labels[name]), ''.join('{:>7}'.format(step) for step in steps))
         for model_label, values_ in zip(labels, values):
             values__ = []
             for min_step, max_step in zip([-1] + steps, steps):
                 a = [value for step, value in values_ if min_step < step <= max_step]
                 if not a:
                     a = None
-                elif args.best and score_name == 'bleu':
+                elif args.best and name == 'bleu':
                     a = max(a)
                 elif args.best:
                     a = min(a)
@@ -182,22 +193,49 @@ if args.txt:
             values_ = values__
 
             try:
-                best_value = max(filter(None, values_)) if score_name == 'bleu' else min(filter(None, values_))
+                best_value = max(filter(None, values_)) if name == 'bleu' else min(filter(None, values_))
             except ValueError:
                 best_value = 0
             s = ['{:>7}'.format('') if x is None else '{:>7.2f}'.format(x) for x in values_]
             s = [boldify(y) if x == best_value else y for x, y in zip(values_, s)]
             print(fmt.format(model_label), ''.join(s))
 else:
-    for label, data_ in zip(labels, data):
-        data_ = [[(step, value) for step, value in data__ if step >= args.min_steps
-                  and (args.max_steps == 0 or step <= args.max_steps)] for data__ in data_]
+    linestyles = [':', '--', '-.']
 
-        for score_name, score_label, linestyle, values in zip(score_names, score_labels, linestyles, data_):
-            if score_name in args.plot and values:
-                plt.plot(*zip(*values), label=' '.join([label, score_label]), linestyle=linestyle)
+    assert not (args.ter and args.bleu) or not args.dev_loss and not args.train_loss
 
-    legend = plt.legend(loc='best', shadow=True)
+    fig, ax_left = plt.subplots()
+    ax_right = ax_left.twinx()
+
+    axes = [ax_left, ax_right, ax_right]
+    axes = axes[:len(args.plot)]
+    ax_left.set_xlabel('steps')
+
+    ax_left.set_ylabel(metric_labels[args.plot[0]])
+    if len(axes) > 1:
+        label = ', '.join(metric_labels[name] for name in args.plot[1:])
+        label = label.replace('dev loss, train loss', 'dev/train loss')
+        ax_right.set_ylabel(label)
+
+    for i, (name, values) in enumerate(data.items()):
+        ax = axes[i]
+        ax.set_prop_cycle(None)
+        linestyle = linestyles[i]
+
+        for values_ in values:
+            values_ = [(step, value) for step, value in values_
+                       if step >= args.min_steps and (args.max_steps == 0 or step <= args.max_steps)]
+
+            ax.plot(*zip(*values_), linestyle=linestyle)
+
+    colors = [line.get_color() for line in ax_left.get_lines()]
+
+    lines = [plt.plot([], [], c=color)[0] for color in colors]
+    lines += [plt.plot([], [], c='black', linestyle=linestyle)[0] for linestyle in linestyles[:len(args.plot)]]
+    labels += [metric_labels[name] for name in data]
+
+    fig.tight_layout()
+    plt.legend(lines, labels, loc='best', shadow=True)
 
     if args.output is not None:
         plt.savefig(args.output)
--- scripts/ssh-plot.sh
+++ scripts/ssh-plot.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -f /tmp/plot.svg
-ssh $1 "rm -f /tmp/plot.svg && plot-loss ${@:2} --output /tmp/plot.svg --no-x"
+ssh $1 "rm -f /tmp/plot.svg && seq2seq/scripts/plot-loss.py ${@:2} --output /tmp/plot.svg --no-x"
 scp $1:/tmp/plot.svg /tmp/ >/dev/null 2>&1
 if [ -f /tmp/plot.svg ]; then
     eog /tmp/plot.svg >/dev/null 2>&1 &
