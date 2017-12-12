ca3d72f93a72d85ea90d408cd0155c2e5c5c97ab
eske <eskaev@gmail.com>
2017-12-12 15:45:27 +0100

    Fixed config files

--- config/AMU17/prepare-2017.sh
+++ config/AMU17/prepare-2017.sh
@@ -19,22 +19,22 @@ for i in {1..20}; do
     cat ${raw_data}/{train,train.2017}.src >> ${data_dir}/train.raw.src
 done
 
-preprocess ${raw_data}/dev.src ${data_dir}/true.en ${data_dir}/en.bpe ${data_dir}/dev.src
-preprocess ${raw_data}/dev.mt ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/dev.mt
-preprocess ${raw_data}/dev.pe ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/dev.pe
+preprocess ${raw_data}/dev.src ${raw_data}/true.en ${raw_data}/en.bpe ${data_dir}/dev.src
+preprocess ${raw_data}/dev.mt ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/dev.mt
+preprocess ${raw_data}/dev.pe ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/dev.pe
 cp ${raw_data}/dev.pe ${data_dir}/dev.raw.pe
 
-preprocess ${raw_data}/test.2016.src ${data_dir}/true.en ${data_dir}/en.bpe ${data_dir}/test.src
-preprocess ${raw_data}/test.2016.mt ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/test.mt
+preprocess ${raw_data}/test.2016.src ${raw_data}/true.en ${raw_data}/en.bpe ${data_dir}/test.src
+preprocess ${raw_data}/test.2016.mt ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/test.mt
 cp ${raw_data}/test.2016.pe ${data_dir}/test.pe
 
-preprocess ${raw_data}/test.2017.src ${data_dir}/true.en ${data_dir}/en.bpe ${data_dir}/test.2017.src
-preprocess ${raw_data}/test.2017.mt ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/test.2017.mt
+preprocess ${raw_data}/test.2017.src ${raw_data}/true.en ${raw_data}/en.bpe ${data_dir}/test.2017.src
+preprocess ${raw_data}/test.2017.mt ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/test.2017.mt
 cp ${raw_data}/test.2017.pe ${data_dir}/test.2017.pe
 
-preprocess ${data_dir}/train.raw.src ${data_dir}/true.en ${data_dir}/en.bpe ${data_dir}/train.src
-preprocess ${data_dir}/train.raw.mt ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/train.mt
-preprocess ${data_dir}/train.raw.pe ${data_dir}/true.de ${data_dir}/de.bpe ${data_dir}/train.pe
+preprocess ${data_dir}/train.raw.src ${raw_data}/true.en ${raw_data}/en.bpe ${data_dir}/train.src
+preprocess ${data_dir}/train.raw.mt ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/train.mt
+preprocess ${data_dir}/train.raw.pe ${raw_data}/true.de ${raw_data}/de.bpe ${data_dir}/train.pe
 
 rm ${data_dir}/train.raw.*
 
