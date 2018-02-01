583c98661cdc53781dce573c5e86d2b3a2a4988d
Alexandre Bérard <alexandre.berard@ed.univ-lille1.fr>
2018-02-01 14:29:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ root_dir=`pwd`
 /usr/bin/env pip3 install tensorflow-gpu python-dateutil pyyaml matplotlib --user --upgrade
 
 cat >>~/.bashrc << EOL
-shopt -s expand_aliases
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 alias get-best-score=${root_dir}/scripts/get-best-score.py
 alias plot-loss=${root_dir}/scripts/plot-loss.py
-alias txt-plot="${root_dir}/scripts/plot-loss.py --txt"
+alias multi-print=${root_dir}/scripts/multi-print.py
 alias copy-model=${root_dir}/scripts/copy-model.py
-alias ssh-plot=${root_dir}/scripts/ssh-plot.sh
 EOL
