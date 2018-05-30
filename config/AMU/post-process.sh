dc2e5cada3dea469a08b98bdd9f9b4a6fda2633f
Alexandre Bérard <eskaev@gmail.com>
2018-05-30 09:35:50 +0200

    Fixed AMU post-processing

--- config/AMU/post-process.sh
+++ config/AMU/post-process.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat "${1:-/dev/stdin}" | sed "s/@@ //g" | scripts/moses/detruecase.perl | scripts/moses/unescape-special-chars.perl
+cat "${1:-/dev/stdin}" | sed "s/@@ //g" | scripts/moses/detruecase.perl | scripts/moses/deescape-special-chars.perl
